0186adebafabfcfa57d8fef80a074168811d8d78
Marsboy02 <marsboy0619@gmail.com>
2023-07-25 13:48:16 +0900

    feat: redis를 사용하도록 설정

--- scraper.ts
+++ scraper.ts
@@ -1,8 +1,16 @@
 import axios from "axios";
 import * as cheerio from "cheerio"
+const redis = require('redis');
 
 const url = "https://www.uos.ac.kr/korNotice/list.do?list_id=FA1";
 const count = 10;
+
+const client = redis.createClient();
+client.connect().then(() => {
+    console.log('Connected to Redis');
+}).catch((err) => {
+    console.log(err.message);
+})
 
 axios.get(url)
 .then(response => {
@@ -11,8 +19,21 @@ axios.get(url)
 
     for (let i = 1; i < count; i++) {
         const element = '#contents > ul > li:nth-child('+ i +') > div > div > div.ti > a'
-        const title = $(element).text();
-        console.log('title:', title);
+        const title = $(element).attr('href');
+
+        const regex = /javascript:fnView\('(\d+)', '(\d+)'\);/;
+        const result = title.match(regex);
+        const key = result[1];
+        const value = result[2];
+        client.set(key,
+            value,
+            (err, reply) => {
+                if (err) {
+                    console.error('Error saving data to Redis:', err);
+                } else {
+                    console.log(`Data saved to Redis with key: ${i}`);
+                }
+            })
     }
 }).catch(error => {
 console.error('Error fetching data: ', error);
