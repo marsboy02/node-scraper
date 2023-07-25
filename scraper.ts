87ac1d1825831b3959e7e04bb49cdeddae38574a
Marsboy02 <marsboy0619@gmail.com>
2023-07-25 12:14:49 +0900

    fix: title을 여러개 크롤링하도록 수정

--- scraper.ts
+++ scraper.ts
@@ -1,14 +1,19 @@
 import axios from "axios";
 import * as cheerio from "cheerio"
 
-const url = "https://www.uos.ac.kr/korNotice/list.do?list_id=FA1"
+const url = "https://www.uos.ac.kr/korNotice/list.do?list_id=FA1";
+const count = 10;
+
 axios.get(url)
 .then(response => {
     const html = response.data;
     const $ = cheerio.load(html);
 
-    const title = $('#contents > ul > li:nth-child(1) > div > div > div.ti > a').text();
-    console.log('title:', title);
+    for (let i = 1; i < count; i++) {
+        const element = '#contents > ul > li:nth-child('+ i +') > div > div > div.ti > a'
+        const title = $(element).text();
+        console.log('title:', title);
+    }
 }).catch(error => {
 console.error('Error fetching data: ', error);
 });
