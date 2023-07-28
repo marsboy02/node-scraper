3c07faaf93ece10778a2d198e7a05d9648590da8
Marsboy02 <marsboy0619@gmail.com>
2023-07-28 16:44:41 +0900

    feat: 4단계 구조로 크롤링 함수 분리

--- scraper.ts
+++ scraper.ts
@@ -1,40 +1,72 @@
 import axios from "axios";
 import * as cheerio from "cheerio"
-const redis = require('redis');
+import Redis from 'ioredis';
+// const redis = require('redis');
+const redis = new Redis({})
 
-const url = "https://www.uos.ac.kr/korNotice/list.do?list_id=FA1";
+const baseUrl = "https://www.uos.ac.kr/korNotice/list.do?list_id=FA1";
 const count = 10;
 
-const client = redis.createClient();
-client.connect().then(() => {
-    console.log('Connected to Redis');
-}).catch((err) => {
-    console.log(err.message);
-})
+async function ReadQueuedUrls() {
+    const index = await redis.lpop('crawl_queue');
+    await QueueContainsUrls(index);
+}
 
-axios.get(url)
-.then(response => {
-    const html = response.data;
-    const $ = cheerio.load(html);
-
-    for (let i = 1; i < count; i++) {
-        const element = '#contents > ul > li:nth-child('+ i +') > div > div > div.ti > a'
-        const title = $(element).attr('href');
-
-        const regex = /javascript:fnView\('(\d+)', '(\d+)'\);/;
-        const result = title.match(regex);
-        const key = result[1];
-        const value = result[2];
-        client.set(key,
-            value,
-            (err, reply) => {
-                if (err) {
-                    console.error('Error saving data to Redis:', err);
-                } else {
-                    console.log(`Data saved to Redis with key: ${i}`);
-                }
-            })
+async function QueueContainsUrls(index: string) {
+    const isMember = await redis.lpos('visited_queue', index);
+    if (!isMember && index) {
+        await CrawlPageAndQueueUrls(index);
     }
-}).catch(error => {
-console.error('Error fetching data: ', error);
-});
+}
+
+async function CrawlPageAndQueueUrls(index: string) {
+    const fullUrl = 'https://www.uos.ac.kr/korNotice/view.do?list_id=FA1' + '&seq=' + index;
+    await axios.get(fullUrl)
+        .then(response => {
+            const html = response.data;
+            const $ = cheerio.load(html);
+            const title = $('#contents > div > div.view-bx > div.vw-tibx > h4').text();
+            const detail = $('#contents > div > div.view-bx > div.vw-tibx > div > div > span').text();
+            console.log('title: ', title);
+            console.log('detail: ', detail);
+            // 웹훅 쏘는 지점
+            }).catch(error => {
+                console.error('Error fetching data: ', error);
+    });
+    await CompleteCrawl(index);
+}
+
+async function CompleteCrawl(index: string) {
+    console.log(index);
+    await redis.rpush('visited_queue', index);
+}
+
+async function CrawlUrls(baseUrl) {
+    axios.get(baseUrl)
+        .then(async response => {
+            const html = response.data;
+            const $ = cheerio.load(html);
+            for (let i = 1; i < count; i++) {
+                const element = '#contents > ul > li:nth-child('+ i +') > div > div > div.ti > a'
+                const title = $(element).attr('href');
+                const regex = /javascript:fnView\('(\d+)', '(\d+)'\);/;
+                const result = title.match(regex);
+                const key = result[1];
+                const value = result[2];
+                await redis.rpush('crawl_queue', result[2]);
+            }
+        }).catch(error => {
+        console.error('Error fetching data: ', error);
+    });
+}
+
+async function main() {
+    await CrawlUrls(baseUrl);
+    await ReadQueuedUrls();
+    await ReadQueuedUrls();
+    await ReadQueuedUrls();
+    await ReadQueuedUrls();
+    await ReadQueuedUrls();
+}
+
+main();
