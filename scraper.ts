63e7a874137c21608dfa28bbe8852effbf599514
Marsboy02 <marsboy0619@gmail.com>
2023-07-28 17:13:54 +0900

    style: 함수 위치 수정

--- scraper.ts
+++ scraper.ts
@@ -2,18 +2,42 @@ import axios from "axios";
 import Redis from 'ioredis';
 import * as cheerio from "cheerio"
 import * as dotenv from 'dotenv';
-const redis = new Redis({})
 
+
+
+// environment
 dotenv.config();
-
 const baseUrl = process.env.BASE_URL;
 const count = 10;
+const redis = new Redis({})
 
+// #0 Crawl Urls
+async function CrawlUrls(baseUrl) {
+    axios.get(baseUrl)
+        .then(async response => {
+            const html = response.data;
+            const $ = cheerio.load(html);
+            for (let i = 1; i < count; i++) {
+                const element = '#contents > ul > li:nth-child('+ i +') > div > div > div.ti > a'
+                const title = $(element).attr('href');
+                const regex = /javascript:fnView\('(\d+)', '(\d+)'\);/;
+                const result = title.match(regex);
+                const index = result[2];
+                await redis.rpush('crawl_queue', index);
+            }
+        }).catch(error => {
+        console.error('Error in #0 CrawlUrls', error);
+    });
+}
+
+
+// #1 Read Queued Urls
 async function ReadQueuedUrls() {
     const index = await redis.lpop('crawl_queue');
     await QueueContainsUrls(index);
 }
 
+// #2 Queue Contains Urls?
 async function QueueContainsUrls(index: string) {
     const isMember = await redis.lpos('visited_queue', index);
     if (isMember == null && index) {
@@ -21,6 +45,7 @@ async function QueueContainsUrls(index: string) {
     }
 }
 
+// #3 Crawl Page and Queue Urls
 async function CrawlPageAndQueueUrls(index: string) {
     const fullUrl = 'https://www.uos.ac.kr/korNotice/view.do?list_id=FA1' + '&seq=' + index;
     await axios.get(fullUrl)
@@ -32,38 +57,20 @@ async function CrawlPageAndQueueUrls(index: string) {
             console.log('title: ', title);
             console.log('detail: ', detail);
             // 웹훅 쏘는 지점
-            }).catch(error => {
-                console.error('Error fetching data: ', error);
+        }).catch(error => {
+            console.error('Error fetching data: ', error);
     });
     await CompleteCrawl(index);
 }
 
+// #4 Complete Crawl and Webhook
 async function CompleteCrawl(index: string) {
     console.log(index);
     await redis.rpush('visited_queue', index);
 }
 
-async function CrawlUrls(baseUrl) {
-    axios.get(baseUrl)
-        .then(async response => {
-            const html = response.data;
-            const $ = cheerio.load(html);
-            for (let i = 1; i < count; i++) {
-                const element = '#contents > ul > li:nth-child('+ i +') > div > div > div.ti > a'
-                const title = $(element).attr('href');
-                const regex = /javascript:fnView\('(\d+)', '(\d+)'\);/;
-                const result = title.match(regex);
-                const key = result[1];
-                const value = result[2];
-                await redis.rpush('crawl_queue', result[2]);
-            }
-        }).catch(error => {
-        console.error('Error fetching data: ', error);
-    });
-}
 
 async function main() {
-    await CrawlUrls(baseUrl);
     await ReadQueuedUrls();
     await ReadQueuedUrls();
     await ReadQueuedUrls();
@@ -71,4 +78,5 @@ async function main() {
     await ReadQueuedUrls();
 }
 
+CrawlUrls(baseUrl);
 main();
