a432bac8031a70edea121b6423a1b137e2abd63c
Marsboy02 <marsboy0619@gmail.com>
2023-07-30 12:50:08 +0900

    style: 함수 분리 및 주석 추가

--- scraper.ts
+++ scraper.ts
@@ -16,21 +16,40 @@ const redis: Redis = new Redis({
     port: +process.env.REDIS_PORT,
 })
 
-/*
-    Producer
-    Crawl Urls into Crawl Queue
+
+/**
+ * take index in list page
  */
-function Producer(baseUrl): void {
-    axios.get(baseUrl)
+function extractIndexWithHtml(i: number, $): string {
+    const element: string = '#contents > ul > li:nth-child(' + i + ') > div > div > div.ti > a'
+    const title = $(element).attr('href');
+    const regex: RegExp = /javascript:fnView\('(\d+)', '(\d+)'\);/;
+    const result = title.match(regex);
+    return result[2];
+}
+
+/**
+ * take detail in each page
+ */
+function extractedPageWithIndex($): void {
+    const title = $('#contents > div > div.view-bx > div.vw-tibx > h4').text();
+    const detail = $('#contents > div > div.view-bx > div.vw-tibx > div > div > span').text();
+    console.log('title: ', title);
+    console.log('detail: ', detail);
+}
+
+
+/**
+ * Producer
+ * Crawl Urls into Crawl Queue
+ */
+function Producer(listUrl): void {
+    axios.get(listUrl)
         .then(async response => {
             const html = response.data;
-            const $ = cheerio.load(html);
+            const $: cheerio.CheerioAPI = cheerio.load(html);
             for (let i = 1; i < producerCount; i++) {
-                const element = '#contents > ul > li:nth-child('+ i +') > div > div > div.ti > a'
-                const title = $(element).attr('href');
-                const regex = /javascript:fnView\('(\d+)', '(\d+)'\);/;
-                const result = title.match(regex);
-                const index = result[2];
+                const index: string = extractIndexWithHtml(i, $);
                 await redis.rpush('crawl_queue', index);
             }
         }).catch(error => {
@@ -38,38 +57,36 @@ function Producer(baseUrl): void {
     });
 }
 
-/*
-   Consumer
-   Read Queued Urls
+
+/**
+ * Consumer
+ * Read Queued Urls
  */
-async function Consumer() {
+async function Consumer(): Promise<void> {
     for (let i = 0; i < consumerCount; i++) { await ReadQueuedUrls() }
 }
 
 
 // #1 Read Urls in Crawl Queue
-async function ReadQueuedUrls() {
-    const index = await redis.lpop('crawl_queue');
+async function ReadQueuedUrls(): Promise<void> {
+    const index: string = await redis.lpop('crawl_queue');
     await QueueContainsUrls(index);
 }
 
 // #2 Visited Queue Already Contains Url?
-async function QueueContainsUrls(index: string) {
+async function QueueContainsUrls(index: string): Promise<void> {
     const isMember = await redis.lpos('visited_queue', index);
     if (isMember == null && index) { await CrawlPageAndQueueUrls(index); }
 }
 
 // #3 Crawl Page and Queue Urls in Visited Queue
-async function CrawlPageAndQueueUrls(index: string) {
+async function CrawlPageAndQueueUrls(index: string): Promise<void> {
     const fullUrl = pageUrl + '&seq=' + index;
     await axios.get(fullUrl)
         .then(response => {
             const html = response.data;
             const $ = cheerio.load(html);
-            const title = $('#contents > div > div.view-bx > div.vw-tibx > h4').text();
-            const detail = $('#contents > div > div.view-bx > div.vw-tibx > div > div > span').text();
-            console.log('title: ', title);
-            console.log('detail: ', detail);
+            extractedPageWithIndex($);
         }).catch(error => {
             console.error('Error fetching data: ', error);
     });
@@ -77,18 +94,18 @@ async function CrawlPageAndQueueUrls(index: string) {
 }
 
 // #4 Complete Crawl and Webhook
-async function CompleteCrawl(index: string) {
+async function CompleteCrawl(index: string): Promise<void> {
     await VisitedUrlsExceedsThreshold();
     await redis.rpush('visited_queue', index);
 }
 
 // #5 Visited Urls Exceeds Threshold?
-async function VisitedUrlsExceedsThreshold() {
-    const visitedQueueLength = await redis.llen('visited_queue');
+async function VisitedUrlsExceedsThreshold(): Promise<void> {
+    const visitedQueueLength: number = await redis.llen('visited_queue');
     if (visitedQueueLength > visitedQueueThreshold) { await redis.lpop('visited_queue') }
 }
 
-function main() {
+function main(): void {
     Producer(listUrl);
     setTimeout(() => Consumer(), 1250);
 }
