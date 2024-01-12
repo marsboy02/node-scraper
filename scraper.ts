8805db77f874ff76c5df4b6846d3b552b5c08894
Marsboy02 <marsboy0619@gmail.com>
2024-01-12 13:33:09 +0900

    feat: log에서 어떤 region에서 에러가 발생했는 지 표시

--- scraper.ts
+++ scraper.ts
@@ -5,6 +5,7 @@ import {
     producerCount,
     consumerTTL,
     webhookUrl,
+    crawlType,
 } from "./config/constants";
 import { triggerWebHook } from "./util/webhook";
 import { Produce, ReadQueuedUrls } from "./util/crawl";
@@ -16,14 +17,14 @@ function main(): void {
     const interval = setInterval(async () => {
         const pageData: PageInterface = await ReadQueuedUrls();
         if (pageData == null) {
-            console.log("queue is empty");
+            console.log("["+ crawlType + "] queue is empty");
         } else {
             triggerWebHook(webhookUrl, pageData);
         }
     }, consumerInterval);
 
     setTimeout((): void => {
-        console.log("container consume done!")
+        console.log("[" + crawlType +"] container consume done!")
         clearInterval(interval);
         process.exit(0);
     }, consumerTTL);
